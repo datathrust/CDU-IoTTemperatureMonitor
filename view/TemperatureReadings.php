1b042a2cc319c7747b6aeca3d459ee67db3bef75
datathrust <shannan@datathrust.net>
2020-03-31 04:13:50 +1030

    Updated Formatting
    Degrees and percentage for Temp/Humid. Closed TD tags.

--- view/TemperatureReadings.php
+++ view/TemperatureReadings.php
@@ -1,3 +1,37 @@
+
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@datathrust
+Learn Git and GitHub without any code!
+
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+datathrust /
+CDU-IoTTemperatureMonitor
+Private
+
+1
+0
+
+    0
+
+Code
+Issues 0
+Pull requests 0
+Actions
+Projects 0
+Wiki
+Security
+Insights
+Settings
+CDU-IoTTemperatureMonitor/view/TemperatureReadings.php /
+@datathrust datathrust Create TemperatureReadings.php 3d4e0ad 5 minutes ago
+33 lines (31 sloc) 716 Bytes
+Code navigation is available!
+
+Navigate your code with ease. Click on function and method calls to jump to their definitions or references in the same repository. Learn more
 <?php
 	echo '
 	<html>
@@ -31,3 +65,18 @@
 		</body>
 	</html>';
 ?>
+
+    © 2020 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
