89c4a1262193b18ef21b9136432fdb16114337ab
datathrust <shannan@datathrust.net>
2020-04-24 04:50:33 +0930

    Update TemperatureReading.php
    Now grabs data from ThingSpeak via cURL.

--- models/TemperatureReading.php
+++ models/TemperatureReading.php
@@ -1,15 +1,15 @@
 <?php
 	Class TemperatureReading
 	{
-	    public $sensorid;
+	    //public $sensorid;
 		public $date;
 		public $time;
 		public $temperature;
 		public $humidity;
 		
-		public function __construct($sensorid = null, $date = null, $time = null, $temperature = null, $humidity = null)
+		public function __construct($date = null, $time = null, $temperature = null, $humidity = null)
 		{
-		    $this -> sensorid = $sensorid;
+		    //$this -> sensorid = $sensorid;
 			$this -> date = $date;
 			$this -> time = $time;
 			$this -> temperature = $temperature;
@@ -18,28 +18,33 @@
 		
 		public function GetAllReadings()
 		{
-            //Database Connection Script
-            //Currently borrowing the painting scores database. Issues with Backend. 
-			include("../../script/581_connect.php");
-			$connection = createConnection();
+			//Set thingspeak URL to pull data from.
+		    $url = "https://api.thingspeak.com/channels/1024699/feeds.json";
+		    
+		    //Run cURL to get JSON data and Decode into PHP array.
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $data = curl_exec($ch);
+            curl_close($ch);
             
-            //Iterate security on this.
-            $sql = "SELECT * FROM SensorReadings ORDER BY DateTime DESC LIMIT 12";
-            $result = $connection -> query($sql);
+            $jsondata = json_decode($data, true);
             $arr = array();
             
-            if ($result -> num_rows > 0 ){
-                while ($row = $result -> fetch_assoc()){
-                    //Explode out date/time.
-                    $dateArray = explode(" ",$row["DateTime"]);
-                    
-                    //Push values to Array
-                    $row["ID"] = new TemperatureReading($row["SensorID"], $dateArray[0], $dateArray[1], $row["Temperature"], $row["Humidity"]);
-                    array_push($arr, $row["ID"]);
-                }
-            }
-           
-           return $arr;
+            foreach($jsondata["feeds"] as $key=>$value){
+                $temp = $value["field1"];
+                $humid = $value["field2"];
+                $datetime =  (explode("T",$value["created_at"])); 
+                
+                $arr[$key] = new TemperatureReading($datetime[0], rtrim($datetime[1], "Z"), $temp, $humid);
+                //array_push($arr, "Something!");
+                
+                //echo $key . "=>" . $value["field1"] . " | " . $value["field2"] ."<br>";
+                //$datetime =  (explode("T",$value["created_at"]));
+                //echo "Date : ". $datetime[0] . " | Time : " . rtrim($datetime[1], "Z");
+                //echo "</br>";
+                    }
+            return $arr;
 		}
 	}
 ?>
