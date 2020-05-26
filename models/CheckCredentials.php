6d8ee0fe4f00bdca50a57497256d693f6eb2292f
datathrust <shannan@datathrust.net>
2020-05-26 23:10:40 +0930

    Update CheckCredentials.php
    Properly utilizes prepared statement for safe database interaction.

--- models/CheckCredentials.php
+++ models/CheckCredentials.php
@@ -3,20 +3,21 @@
 
     function CheckCredentials($user, $pass)
     {
-        //Connect to database, get username and hashed password from Database.
-        $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-        $sql = "SELECT * FROM users WHERE username = '$user'";
-        $result = mysqli_query($db, $sql);
-        
-        //Map query results to array, use password_verify to check for correct hash.
-            if(mysqli_num_rows($result) > 0){
-                while($row = mysqli_fetch_array($result)){
-                    if(password_verify($pass, $row["password"])){
-                        return 1;
-                    }
-                }
+        // Connect to Database, ustilize prepared statement to get Hash from database for provided username. 
+        $connection = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+        $statement = $connection -> prepare ("SELECT * FROM users WHERE username = ?");
+        $statement -> bind_param("s",$user);
+        $statement -> execute();
+        $result = $statement -> get_result();
+
+        //If the username appears in the return query, compare provided password with stored hash.
+        while ($row = $result->fetch_assoc())
+        {
+            if(password_verify($pass, $row["password"])){
+            return 1;
             }
-            
+        }
+
         //If no matches, or invalid hash.
         return 0;
     }
